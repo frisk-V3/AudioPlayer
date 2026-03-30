69fa97841eef871d02ea1317da09c94f647e851e
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-03-30 14:01:33 +0900

    Delete apps directory

--- apps/desktop/preload.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onOpenFile: (callback: (file: string) => void) => {
-    ipcRenderer.on('open-file', (_, file) => callback(file));
-  }
-});
